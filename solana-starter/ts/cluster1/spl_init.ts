c21950b8148add40815ab06c67c64be96aa0f4d6
Godwin Idemudia <rustoshidev@gmail.com>
2025-10-07 18:55:03 +0100

    completed spl token init and mint task

--- solana-starter/ts/cluster1/spl_init.ts
+++ solana-starter/ts/cluster1/spl_init.ts
@@ -2,19 +2,18 @@ import { Keypair, Connection, Commitment } from "@solana/web3.js";
 import { createMint } from '@solana/spl-token';
 import wallet from "/home/rustoshidev/.config/solana/id.json";
 
-console.log(wallet);
-
 // Import our keypair from the wallet file
 const keypair = Keypair.fromSecretKey(new Uint8Array(wallet));
 
-//Create a Solana devnet connection
+// //Create a Solana devnet connection
 const commitment: Commitment = "confirmed";
 const connection = new Connection("https://api.devnet.solana.com", commitment);
 
 (async () => {
     try {
         // Start here
-        // const mint = ???
+        const mint = await createMint(connection, keypair, keypair.publicKey, null, 6);
+        console.log("Mint created:", mint.toBase58());
     } catch(error) {
         console.log(`Oops, something went wrong: ${error}`)
     }
