b5cd10ddbe76d731e7242937f13ed46444e5dcf7
Godwin Idemudia <rustoshidev@gmail.com>
2025-10-08 15:55:58 +0100

    completed spl metadata and transfer

--- solana-starter/ts/cluster1/spl_metadata.ts
+++ solana-starter/ts/cluster1/spl_metadata.ts
@@ -1,4 +1,4 @@
-import wallet from "../turbin3-wallet.json"
+import wallet from "/home/rustoshidev/.config/solana/id.json";
 import { createUmi } from "@metaplex-foundation/umi-bundle-defaults"
 import { 
     createMetadataAccountV3, 
@@ -10,7 +10,7 @@ import { createSignerFromKeypair, signerIdentity, publicKey } from "@metaplex-fo
 import { bs58 } from "@coral-xyz/anchor/dist/cjs/utils/bytes";
 
 // Define our Mint address
-const mint = publicKey("<mint address>")
+const mint = publicKey("5bnBNhoRgSDbkDiUHRHhgWGY4aifBRK6kspUCcEsW6YE")
 
 // Create a UMI connection
 const umi = createUmi('https://api.devnet.solana.com');
@@ -21,28 +21,39 @@ umi.use(signerIdentity(createSignerFromKeypair(umi, keypair)));
 (async () => {
     try {
         // Start here
-        // let accounts: CreateMetadataAccountV3InstructionAccounts = {
-        //     ???
-        // }
+        let accounts: CreateMetadataAccountV3InstructionAccounts = {
+            mint: mint,
+            mintAuthority: signer,
+            payer: signer,
+            updateAuthority: keypair.publicKey
+        };
 
-        // let data: DataV2Args = {
-        //     ???
-        // }
+        let data: DataV2Args = {
+            name: "Godwyn Token",
+            symbol: "GYW",
+            sellerFeeBasisPoints: 500,
+            uri: "http://mytoken.com",
+            collection: null,
+            creators: null,
+            uses: null
+        }
 
-        // let args: CreateMetadataAccountV3InstructionArgs = {
-        //     ???
-        // }
+        let args: CreateMetadataAccountV3InstructionArgs = {
+            collectionDetails: null,
+            data: data,
+            isMutable: true
+        }
 
-        // let tx = createMetadataAccountV3(
-        //     umi,
-        //     {
-        //         ...accounts,
-        //         ...args
-        //     }
-        // )
+        let tx = createMetadataAccountV3(
+            umi,
+            {
+                ...accounts,
+                ...args
+            }
+        )
 
-        // let result = await tx.sendAndConfirm(umi);
-        // console.log(bs58.encode(result.signature));
+        let result = await tx.sendAndConfirm(umi);
+        console.log(bs58.encode(result.signature));
     } catch(e) {
         console.error(`Oops, something went wrong: ${e}`)
     }
--- solana-starter/ts/cluster1/spl_transfer.ts
+++ solana-starter/ts/cluster1/spl_transfer.ts
@@ -1,5 +1,5 @@
-import { Commitment, Connection, Keypair, LAMPORTS_PER_SOL, PublicKey } from "@solana/web3.js"
-import wallet from "../turbin3-wallet.json"
+import { Commitment, Connection, Keypair, PublicKey } from "@solana/web3.js"
+import wallet from "/home/rustoshidev/.config/solana/id.json";
 import { getOrCreateAssociatedTokenAccount, transfer } from "@solana/spl-token";
 
 // We're going to import our keypair from the wallet file
@@ -10,18 +10,27 @@ const commitment: Commitment = "confirmed";
 const connection = new Connection("https://api.devnet.solana.com", commitment);
 
 // Mint address
-const mint = new PublicKey("<mint address>");
+const mint = new PublicKey("5bnBNhoRgSDbkDiUHRHhgWGY4aifBRK6kspUCcEsW6YE");
 
 // Recipient address
-const to = new PublicKey("<receiver address>");
+const to = new PublicKey("G7MTCM2S1W6ufPhYLjodUyRZLBFbPz91CXd5C63aWoqV");
 
 (async () => {
     try {
         // Get the token account of the fromWallet address, and if it does not exist, create it
-
+        const fromTokenAccount = await getOrCreateAssociatedTokenAccount(connection, keypair, mint, keypair.publicKey);
         // Get the token account of the toWallet address, and if it does not exist, create it
-
+        const toTokenAccount = await getOrCreateAssociatedTokenAccount(connection, keypair, mint, to);
         // Transfer the new token to the "toTokenAccount" we just created
+        const transferTx = await transfer(
+            connection, 
+            keypair, 
+            fromTokenAccount.address, 
+            toTokenAccount.address, 
+            keypair.publicKey, 
+            5000 * 1e6
+        );
+        console.log(transferTx);
     } catch(e) {
         console.error(`Oops, something went wrong: ${e}`)
     }
